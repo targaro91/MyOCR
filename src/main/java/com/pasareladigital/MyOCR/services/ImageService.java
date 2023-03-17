8d00bf22546e54be2a6fcb1f857f0473cf312e0a
Antonio David Perera Calás <david@alimatic.cu>
2023-03-17 13:59:30 -0400

    cambios

--- src/main/java/com/pasareladigital/MyOCR/services/ImageService.java
+++ src/main/java/com/pasareladigital/MyOCR/services/ImageService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 
+import net.sourceforge.tess4j.ITessAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -24,21 +25,19 @@ public class ImageService {
         final String pathTessdata = env.getProperty("tessdata")+"/";
         tesseract.setDatapath(pathTessdata); // Cambia esto por la ruta a la carpeta tessdata en tu proyecto
         tesseract.setLanguage("spa"); // Establece el idioma del texto a reconocer
-
+        //final String value = ",.:; áéíóúabcdefghijklmnñopqrstuvwxyz0123456789ÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        //tesseract.setTessVariable("tessedit_char_whitelist", value); //Especifica q solo esos caracteres seran reconocidos
+        tesseract.setOcrEngineMode(ITessAPI.TessOcrEngineMode.OEM_LSTM_ONLY); //Especifica el motor usado
     }
 
     public String convertImageToText(String imagePath) throws TesseractException {
-
 
         // Carga la imagen
         File imageFile = new File(imagePath);
 
         // Convierte la imagen a texto
         String result="";
-
         result = this.tesseract.doOCR(imageFile);
-
-
         return result;
     }
 
